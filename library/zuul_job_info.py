7ad5d4b255f0fe8d051af7b8e57eec80cb84f752
“David <dsariel@redhat.com>
2023-08-09 16:48:08 +0300

    Send Zuul job status and test case reports to ReportPortal.
    This update introduces Ansible modules "zuul_job_info" and
    "zuul_tests_info". Additionally, it involves code refactoring
    by centralizing common functions and exception classes within
    "utils.py" and "exceptions.py" files.
    
    The modifications to the "main.yaml" playbook include:
    
    - Retrieving test results for a designated build from a Zuul CI.
    - Gathering job status and testcase statuses in XML format.
    - Forwarding XML files to ReportPortal.
    - Retrieving test suite and test case information from ReportPortal
      for verification purposes.
    
    Change-Id: I2b56dcaaaf57bb2a27e6a3d6861eecb7eb4f3213

---
+++ library/zuul_job_info.py
@@ -0,0 +1,159 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2023, RedHat
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import json
+import os
+import requests
+import urllib.request
+import xml.etree.ElementTree as ET
+import sys
+
+   
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.utils import get_json
+
+from lxml import etree
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from urllib.error import HTTPError
+
+
+DOCUMENTATION = '''
+module: zuul_job_info
+version_added: '2.9'
+short_description: Get Zuul job details
+description:
+    - This module creates an XML file using the provided Zuul job details,
+	  including information such as duration, start time, and job status.
+	  The resulting XML file is then stored in the designated destination.
+      Job details are collected from the Zuul server using pipeline REST API.
+options:
+    zuul_domain:
+        description: URL of the Zuul server
+        required: True
+        type: str 
+    zuul_tenant:
+        description: Zuul tenant
+        required: True
+        type: str    
+    zuul_job_build_id:
+        description: ID of the job build
+        required: True
+        type: str
+    zuul_api_path_template:
+        description: Zuul API path template to extract job info
+        E.g.:
+           "{zuul_domain}/api/tenant/{zuul_tenant}/build/{zuul_job_build_id}"
+        required: True
+        type: str
+    ssl_verify:
+        description: set certification verifications on/off
+        default: True
+        type: bool
+    output_xml_file:
+        description: File path to save the generated XML file
+        required: True
+        type: str
+
+requirements:
+    - "datetime"
+    - "json"
+    - "os"
+    - "requests"
+    - "urllib"
+    - "xml"
+'''
+
+RETURN = '''
+file_path:
+    description: Path of the saved XML files
+    type: string
+    returned: always
+'''
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+ssl_verify = True
+
+def create_test_suite(base_url):
+    response = get_json(f'{base_url}', ssl_verify)
+
+    suite = etree.Element('testsuite')
+    suite.set('name', 'deployment')
+    suite.set('time', str(response.get('duration', 0)))
+    suite.set('result', str(response.get('result', 0)))
+
+    date_string = str(response.get('start_time', 0)) # "2023-08-06T13:20:14"
+    date_format = f'%Y-%m-%dT%H:%M:%S'
+    suite.set('timestamp', convert_date_to_sec(date_string,date_format))
+
+    return suite
+   
+def main():
+    result = {}
+    module_args = dict(zuul_domain=dict(type='str', required=True),
+                       zuul_tenant=dict(type='str', required=True),
+                       zuul_job_build_id=dict(type='str', required=True),
+                       zuul_api_path_template=dict(type='str', required=True),
+                       ssl_verify=dict(type='bool', default=True),
+                       output_xml_file=dict(type='str', required=True))
+    module = AnsibleModule(argument_spec=module_args,
+                           supports_check_mode=False)
+    try:
+        global ssl_verify
+        zuul_domain = module.params.pop('zuul_domain')
+        zuul_tenant = module.params.pop('zuul_tenant')
+        zuul_job_build_id = module.params.pop('zuul_job_build_id')
+        zuul_api_path_template=module.params.pop('zuul_api_path_template')
+        ssl_verify = module.params.pop('ssl_verify')
+        output_xml_file = module.params.pop('output_xml_file')
+
+        base_url = zuul_api_path_template.format(zuul_domain=zuul_domain, \
+                                               zuul_tenant=zuul_tenant, \
+                                                zuul_job_build_id=zuul_job_build_id)
+
+        suite = create_test_suite(base_url)
+        result['file_path'] = save_to_file(suite, output_xml_file)
+
+        module.exit_json(**result)
+    except Exception as ex:
+        result['msg'] = ex
+        module.fail_json(**result)
+
+if __name__ == '__main__':
+
+    #main()
+    
+    result = {}
+    module_args = dict(zuul_domain=dict(type='str', required=True),
+                       zuul_tenant=dict(type='str', required=True),
+                       zuul_job_build_id=dict(type='str', required=True),
+                       zuul_api_path_template=dict(type='str', required=True),
+                       ssl_verify=dict(type='bool', default=True),
+                       output_xml_file=dict(type='str', required=True))
+    module = AnsibleModule(argument_spec=module_args,
+                           supports_check_mode=False)
+    # module.add_path_info(str(__file__))
+
+    
+    result['file_path'] = __file__
+    result['sys_path'] = sys.path
+    stream = os.popen('find . -name utils')
+    result['tree'] = stream.read()
+    module.exit_json(**result)
+    
